1408e76567328a11f3bf1cfa1c0ad8325b9ca283
Webbenemies <130279128+Webbenemies@users.noreply.github.com>
2024-02-05 18:17:53 +0530

    Add files via upload

---
+++ servar.js
@@ -0,0 +1,25 @@
+console.log('>>>>>>>>>>>', "server")
+import express from "express"
+import { createServer } from "node:http"
+import { fileURLToPath } from "node:url"
+import { dirname, join } from "node:path"
+import { Server } from "socket.io"
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
+const name = dirname(fileURLToPath(import.meta.url))
+
+app.get("/",(req, res)=>{
+res.sendFile(join(name, "home.html"))
+})
+
+io.on("connection",(socket)=>{
+    socket.on("chat",(e)=>{
+        io.emit("chat", e)
+    })
+})
+
+server.listen(3000,()=>{
+    console.log("server is run");
+})
